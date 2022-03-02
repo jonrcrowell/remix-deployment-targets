07d8aa5b54ee807b7e40cf6c7656397f82b4ab7e
Jon Crowell <jon.crowell@stonegiantstudio.com>
2022-03-02 16:54:43 -0600

    switch from remix server to netlify

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: "netlify/functions/server/index.js",
   // publicPath: "/build/",
   // devServerPort: 8002
 };
